1aec82c25e8b4abe364648ed946ca9b1758ff9b6
Tammer Abiyu <tammerabiyu@gmail.com>
2014-03-31 16:47:30 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Renshuu::Application.config.secret_key_base = '2fdb3972a4e60629b04434d1c8a5f7a1b91db09489e2cf5d1366f0f874fc275b20baf693f416e9fe5416e7a74e85cc5c1298379ab4ddde7e67de96348500098d'
